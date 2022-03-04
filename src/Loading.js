5aca4c41921d620c2cb079f42202988eeba071fa
Morgan VanYperen <morgvanny@gmail.com>
2022-03-04 11:27:26 -0800

    added loading gif

--- src/Loading.js
+++ src/Loading.js
@@ -1,5 +1,10 @@
 import React from "react";
 
 export default function Loading() {
-  return <div>Loading...</div>;
+  return (
+    <div>
+      <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/29/Loader.gif/480px-Loader.gif" />
+      <p>Loading...</p>
+    </div>
+  );
 }
