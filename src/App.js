e9b44aca9347f6ec0ce3961d8ec0835992fca959
Morgan VanYperen <morgvanny@gmail.com>
2022-03-04 11:14:16 -0800

    starting point

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState, useEffect } from "react";
+import Loading from "./Loading";
+import Profile from "./Profile";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [done, setDone] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setDone(true);
+    }, 2500);
+  }, []);
+
+  return <div className="App">{done ? <Profile /> : <Loading />}</div>;
 }
 
 export default App;
---
+++ src/Profile.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function () {
+  return (
+    <div>
+      <p>Name: Morgan</p>
+      <p>Occupation: Software Engineering Instructor</p>
+    </div>
+  );
+}
