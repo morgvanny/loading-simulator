f49e2e8ab182d68f1eede7a49742cf3ae3454a26
Morgan VanYperen <morgvanny@gmail.com>
2022-03-04 11:33:54 -0800

    added loading to profile

--- src/Profile.js
+++ src/Profile.js
@@ -1,11 +1,12 @@
 import React from "react";
-
+import Loading from "./Loading";
 export default function () {
   return (
     <div>
       <img src="https://ca.slack-edge.com/T02MD9XTF-UANUM7SUR-849b7bef2aee-512" />
       <p>Name: Morgan</p>
       <p>Occupation: Software Engineering Instructor</p>
+      <Loading />
     </div>
   );
 }
