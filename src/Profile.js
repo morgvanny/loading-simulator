98306d0500822c12fc0c41e78e0bd2270f1403ef
Morgan VanYperen <morgvanny@gmail.com>
2022-03-04 11:24:11 -0800

    added pic to profile

--- src/Profile.js
+++ src/Profile.js
@@ -3,6 +3,7 @@ import React from "react";
 export default function () {
   return (
     <div>
+      <img src="https://ca.slack-edge.com/T02MD9XTF-UANUM7SUR-849b7bef2aee-512" />
       <p>Name: Morgan</p>
       <p>Occupation: Software Engineering Instructor</p>
     </div>
